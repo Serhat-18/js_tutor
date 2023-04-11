37d1f798a21f3167bb588ca1c47383d9cefd3eb6
Serhat-18 <79026362+Serhat-18@users.noreply.github.com>
2023-04-11 17:41:50 +0300

    Delete print.js

--- print/print.js
+++
@@ -1,8 +0,0 @@
-const hawliseroprint = {
-    veri1: "Serhat",
-    veri2: "20",
-    veri3: "Bu tarzda arttırabilirsiniz ^^ "
-}
-
-console.log(hawliseroprint)
-console.log("ŞUNU ARTIK ANLAYIN 18 YAŞINDA DEĞİLİM O BENİM NİCKNAMEM XKRALTR GİBİ DÜŞÜNÜN")
